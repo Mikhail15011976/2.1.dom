4e73dcd9376c1db0e6c9b24229da778f36d43238
Михаил Бабков <nebushko.mikhail@gmail.com>
2025-01-31 14:05:05 +0300

    second

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,37 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-];
+/** @type {import('eslint').Linter.Config} */
+export default {
+  languageOptions: {
+    globals: {
+      ...globals.node,
+      module: 'readonly',
+      require: 'readonly',
+      __dirname: 'readonly',
+    },
+    parserOptions: {
+      ecmaVersion: 2020,
+    },
+  },
+  rules: {
+    // Здесь вы можете добавить свои специфические правила
+  },
+  overrides: [
+    {
+      files: ['*.js'],
+      languageOptions: {
+        globals: {
+          ...globals.node,
+        },
+      },
+    },
+  ],
+  extends: [
+    pluginJs.configs.recommended,
+  ],
+  env: {
+    node: true,
+    browser: false,
+  },
+};
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,22 +1,18 @@
-const webpack = require("webpack");
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common");
+import webpack from "webpack";
+import { merge } from "webpack-merge";
+import common from "./webpack.common.js";
 
-module.exports = merge(common, {
-  // Set the mode to development or production
-  mode: "development",
-  // Control how source maps are generated
+export default merge(common, {  
+  mode: "development",  
   devtool: "inline-source-map",
-
-  // Spin up a server for quick development
+  
   devServer: {
     historyApiFallback: true,
     open: true,
     compress: true,
   },
 
-  plugins: [
-    // Only update what has changed on hot reload
+  plugins: [    
     new webpack.HotModuleReplacementPlugin(),
   ],
 });
